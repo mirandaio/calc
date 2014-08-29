45932c89d97c02fcbc4ec8caed5bdfc0ef97904f
Alfredo Miranda <alfredo@miranda.io>
2014-08-29 00:24:31 -0500

    Add function descriptions

--- calc.py
+++ calc.py
@@ -1,10 +1,30 @@
 import sys
 
-def tokenize():
+def tokenize(expr):
+    """
+    expr: a string representing a mathematical expression in infix notation
+
+    returns: a list of strings where each string represents a token of the 
+    mathematical expression
+    """
     pass
 
-def topostfix():
+def topostfix(tokens):
+    """
+    tokens: a list of strings which represent the tokens of a mathematical 
+    expression in infix notation
+
+    returns: a queue of strings that represent the tokens of a mathematical 
+    expression in postfix notation
+    """
     pass
 
-def eval():
+def eval(token):
+    """
+    tokens: a queue of strings that represent the tokens of a mathematical
+    expression in postfix notation
+
+    returns: a floating number that is the result of evaluating the
+    mathematical expression
+    """
     pass
