0d53953f09a739001bc364df9941e2f2635ddff6
Alfredo Miranda <alfredo@miranda.io>
2014-08-29 21:15:56 -0500

    Add valid input tests

--- calctest.py
+++ calctest.py
@@ -1,6 +1,33 @@
 import unittest
 import calc
 
+class TokenizeValidInput(unittest.TestCase):
+    valid_expr = (
+        ("", []),
+        ("()", ["(", ")"]),
+        ("(())", ["(", "(", ")", ")"]),
+        ("()()", ["(", ")", "(", ")"]),
+        ("1", ["1"]),
+        ("5.84", ["5.84"]),
+        ("-1", ["_", "1"]), # unary negation is represented as _
+        ("log 14", ["log", "14"]),
+        ("log(3)", ["log", "(", "3", ")"]),
+        ("(2)", ["(", "2", ")"]),
+        ("1 + 2", ["1", "+", "2"]),
+        ("1/0", ["1", "/", "0"]),
+        ("2.3+1", ["2.3", "+", "1"]),
+        ("4.5 + 1 * 4.9", ["4.5", "+", "1", "*", "4.9"]),
+        ("(23 + 5) * 5", ["(", "23", "+", "5", ")", "*", "5"]),
+        ("12*(56+4)", ["12", "*", "(", "56", "+", "4", ")"]),
+        ("log3+1", ["log", "3", "+", "1"]),
+        ("log(4.1 + 2)", ["log", "(", "4.1", "+", "2", ")"]),
+        ("log(1 + 7) + 4", ["log", "(", "1", "+", "7", ")", "+", "4"]))
+
+    def test_valid_expr(self):
+        for expr, tokens in self.valid_expr:
+            result = calc.tokenize(expr)
+            self.assertEqual(tokens, result)
+
 class TokenizeBadInput(unittest.TestCase):
     invalid_expr = (
         "1 1",         # consecutive numbers without operator in middle
@@ -16,7 +43,7 @@ class TokenizeBadInput(unittest.TestCase):
         "(1 + 2 * 3",  # unbalanced parentheses
         "1 + 2) * 3") # unbalanced parentheses
 
-    def test_invalid_expression(self):
+    def test_invalid_expr(self):
         for expr in self.invalid_expr:
             self.assertRaises(calc.InvalidExpression, calc.tokenize, expr)
 
