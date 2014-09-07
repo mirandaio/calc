4f4c6ca9c3aecabd39a6c3cd0079f85b8a45fe23
Alfredo Miranda <alfredo@miranda.io>
2014-09-07 18:06:37 -0500

    Modified the output formatting

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-var session = '';
+var sfront = [];
+var sback = [];
 
 $(document).ready(function() {
   $('input').keydown(function(event) {
     if(event.which == 13) {
       expr = $(this).val();
       $(this).val('');
-      $.post('/', {'expr': expr, 'session': session}, function(data) {
-
-        session += '\nInput : ' + expr + '\nOutput: ' + data;
-        $('.session').text(session);
+      $.post('/', {'expr': expr, 'session': sback.join("")}, function(data) {
+        sfront.push('\ninput : ', expr, '\noutput: ', data);
+        sback.push('\n', expr, '\n=', data);
+        $('.session').text(sfront.join(""));
       });
     }
   });
