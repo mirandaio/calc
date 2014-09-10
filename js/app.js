298c64bc0213e11a7c38e96afdd2a6cc6e05fe9a
Alfredo Miranda <alfredo@miranda.io>
2014-09-10 10:08:50 -0500

    Oops, forgot var

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var sback = [];
 $(document).ready(function() {
   $('input').keydown(function(event) {
     if(event.which == 13) {
-      expr = $(this).val();
+      var expr = $(this).val();
       $(this).val('');
       $.post('/', {'expr': expr, 'session': sback.join("")}, function(data) {
         sfront.push('\ninput : ', expr, '\noutput: ', data);
